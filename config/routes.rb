e85e8f55e1a4b732fc077a7a66292b467c3df550
kiichifukuzawa <back19future85f@outlook.jp>
2020-02-26 00:54:07 +0900

    詳細画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # 投稿のルーティング
-  get "posts/index"
+  get "posts/index" => "posts#index"
+  get "posts/:id" => "posts#show"
 
   # homeのルーティング
   get "/" => "home#top"
