26838892aefd9ac1fbcbd0e5658d65c6f20a6a34
kiichifukuzawa <back19future85f@outlook.jp>
2020-03-02 15:33:21 +0900

    ログイン中の新規登録やログインページ、トップページへのアクセス制限をかけた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,14 @@ class ApplicationController < ActionController::Base
       redirect_to("/login")
     end
   end
-  
+
+ # ログイン済みのユーザーに対するアクセス制限
+ def forbid_login_user
+   if @current_user
+     flash[:notice] = "すでにログインしています"
+     redirect_to("/posts/index")
+   end
+ end
+
+
 end
