a8f078ef64a0b8a50b20a73294b1b9e16770caed
kiichifukuzawa <back19future85f@outlook.jp>
2020-03-02 18:02:49 +0900

    投稿の編集と削除のアクセス権限

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 before_action :authenticate_user
+before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
 
 
   # 投稿一覧
@@ -58,4 +59,14 @@ before_action :authenticate_user
     redirect_to("/posts/index")
   end
 
+  # アクセスの制限
+  def ensure_correct_user
+    @post = Post.find_by(id: params[:id])
+    if @post.user_id != @current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
+  end
+
+  
 end
